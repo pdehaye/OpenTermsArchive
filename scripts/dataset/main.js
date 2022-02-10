3472230f68944d60dd95009177a3cf29b7a4e719
Nicolas Dupont <npg.dupont@gmail.com>
2022-02-10 09:02:56 +0100

    Improve variable naming
    Co-authored-by: Matti Schneider <matti@opentermsarchive.org>

--- scripts/dataset/main.js
+++ scripts/dataset/main.js
@@ -6,17 +6,17 @@ import { release } from './index.js';
 
 (async () => {
   const args = process.argv.slice(2);
-  const publicationEnabled = args.includes('--publish');
-  const removeLocalCopyEnabled = args.includes('--remove-local-copy');
-  const scheduleEnable = args.includes('--schedule');
+  const shouldPublish = args.includes('--publish');
+  const shouldRemoveLocalCopy = args.includes('--remove-local-copy');
+  const shouldSchedule = args.includes('--schedule');
   const argsWithoutOptions = args.filter(arg => !arg.startsWith('--'));
   const [fileName] = argsWithoutOptions;
 
-  if (!scheduleEnable) {
-    return release({ publicationEnabled, removeLocalCopyEnabled, fileName });
+  if (!shouldSchedule) {
+    return release({ shouldPublish, shouldRemoveLocalCopy, fileName });
   }
 
   logger.info('Release will be created every Monday at 03h30');
 
-  scheduler.scheduleJob({ hour: 3, minute: 30, dayOfWeek: 1 }, async () => release({ publicationEnabled, removeLocalCopyEnabled, fileName }));
+  scheduler.scheduleJob({ hour: 3, minute: 30, dayOfWeek: 1 }, async () => release({ shouldPublish, shouldRemoveLocalCopy, fileName }));
 })();
